7c9ee4e2404efea39d5bd57ae6e120e01a9a4eb4
Practice <hcxiaoling@163.com>
2024-12-03 19:30:20 +0800

    2024_12_3学习资料

---
+++ 唐老狮Unity教程/代码-唐老狮Unity教程/TangC#Unity_2024_12_3/TangC#Unity_2024_12_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+///三杠注释一般用来注释一些函数
+///
+Console.WriteLine("大伟的从零开始游戏之旅");//WriteLine自动换行
+
+Console.Write("你好");//Write不空行
+Console.Write("你好\n");
+
+Console.WriteLine("请给你的角色起名");
+
+Console.ReadLine();
+
+Console.WriteLine("名称输入完毕");
+
+Console.ReadKey();//检测一个按键
+
+Console.WriteLine("单键输入完毕");
+
+
+
---
+++ 唐老狮Unity教程/代码-唐老狮Unity教程/TangC#Unity_2024_12_3/Test_Lesson1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("你的名字");
+
+Console.ReadLine();
+
+Console.WriteLine("你的年龄");
+
+Console.ReadLine();
+
+Console.WriteLine("你的班级");
+
+Console.ReadLine();
+
+Console.WriteLine("你最喜欢的运动");
+
+Console.ReadLine();
+
+Console.WriteLine("哈哈，好巧我也喜欢这个运动");
+
+
+
+
+int size = 10;
+
+for (int i = 0; i < size; i++)  // 遍历每一行
+{
+    for (int j = 0; j < size; j++)  // 遍历每一列
+    {
+        // 判断边界：第一行、最后一行、第一列、最后一列
+        if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+        {
+            Console.Write("*");  // 边界位置输出星号
+        }
+        else
+        {
+            Console.Write(" ");  // 内部位置输出空格
+        }
+    }
+    Console.WriteLine();  // 输出完一行后换行
+}
